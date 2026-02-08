c32571957b9061ec2cb9ae29f94abb7e9d04cbc0
Ryan Ly <contemporaryaxis@gmail.com>
2026-02-07 23:09:04 -0500

    Completed header styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,30 +2,44 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-black text-white h-28 px-2">
-      <div className="h-full flex items-end justify-between pb-2">
-        {/* Logo - aligned to bottom */}
-        <div className="flex items-end gap-3 h-full">
+    <header className="bg-black text-white h-32 px-3">
+      <div className="h-full flex items-center justify-between">
+        {/* Logo */}
+        <div className="flex items-center gap-3 mt-6">
           <Image 
             src="/logo_text.png" 
             alt="ganttstop logo" 
-            width={220} 
+            width={190} 
             height={60}
             className="object-contain"
           />
         </div>
         
-        {/* Navigation - also aligned to bottom */}
-        <nav className="flex gap-8 pb-1">
-          <a href="/" className="hover:text-gray-300 transition">HOME</a>
-          <a href="/my-gantts" className="hover:text-gray-300 transition">MY GANTTS</a>
-          <a href="/customization" className="hover:text-gray-300 transition">CUSTOMIZATION</a>
-          <a href="/help" className="hover:text-gray-300 transition">HELP</a>
+        {/* Navigation - centered but pushed down slightly */}
+        <nav className="flex gap-50 items-center mr-15 mt-6">
+          <a href="/" className="hover:opacity-70 transition">
+            <Image src="/home.png" alt="HOME" width={70} height={25} className="object-contain" />
+          </a>
+          <a href="/my-gantts" className="hover:opacity-70 transition">
+            <Image src="/my-gantts.png" alt="MY GANTTS" width={120} height={25} className="object-contain" />
+          </a>
+          <a href="/customization" className="hover:opacity-70 transition">
+            <Image src="/customization.png" alt="CUSTOMIZATION" width={170} height={25} className="object-contain" />
+          </a>
+          <a href="/help" className="hover:opacity-70 transition">
+            <Image src="/help.png" alt="HELP" width={65} height={25} className="object-contain" />
+          </a>
         </nav>
         
-        {/* User icon - also aligned to bottom */}
-        <div className="pb-1">
-          <div className="w-8 h-8 bg-white rounded-full"></div>
+        {/* User profile icon - also pushed down */}
+        <div className="mr-15 mt-6">
+          <Image 
+            src="/default-profile.png" 
+            alt="Profile" 
+            width={85} 
+            height={40}
+            className="rounded-full object-cover"
+          />
         </div>
       </div>
     </header>
